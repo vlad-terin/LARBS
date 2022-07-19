99e3ff19512f212040e986beda1d7ec50211e76b
Vlad Terin <vladterin@gmail.com>
2022-07-18 19:17:56 -0500

    Update larbs.sh

--- larbs.sh
+++ larbs.sh
@@ -6,8 +6,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+dotfilesrepo="https://github.com/vlad-terin/voidrice.git"
+progsfile="https://raw.githubusercontent.com/vlad-terin/LARBS/master/progs.csv"
 aurhelper="yay"
 repobranch="master"
 
